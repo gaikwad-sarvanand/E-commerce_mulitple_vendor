03ce8648ed4989f84424b924c859ff45daffb371
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-20 13:52:11 +0530

    completed creating virtual env and basic setup and core app basic

---
+++ e_commerce/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from core.views import frontpage,about
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',frontpage,name="frontpage"),
+    path('about_page/',about,name="about")
+]
