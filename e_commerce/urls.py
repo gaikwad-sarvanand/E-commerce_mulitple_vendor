15c45a59512f827165495b8405dc4645cb33dbf1
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-21 12:16:44 +0530

    Category View Completed

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import frontpage,about
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('about_page/',about,name="about"),
+    path('',include('store.urls')),
     path('',frontpage,name="frontpage"),
-    path('about_page/',about,name="about")
+    path('admin/', admin.site.urls),
+
+    
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from.models import Product,Category
 
 # Create your views here.
+
+def category_detail(request,slug):
+    categroy = get_object_or_404(Category , slug=slug)
+    products = categroy.products.all()
+    return render(request,"store/category_detail.html",{'category':categroy,'products':products})
+
+def product_details(request,category_slug, slug):
+    product =get_object_or_404( Product,slug=slug)
+    context = {"product":product}
+    return render(request,"store/product_detail.html",context)
