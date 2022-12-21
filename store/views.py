e7c8dd8c321abcf16bec6cc181bfe3c611c90cab
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-21 14:57:33 +0530

    Done with seach bar query

--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render,get_object_or_404
+from django.db.models import Q
 from.models import Product,Category
 
 # Create your views here.
+def search(request):
+    query = request.GET.get('query','')
+    product = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    
+    return render(request,"store/search.html",{'query':query,'products':product})
 
 def category_detail(request,slug):
     categroy = get_object_or_404(Category , slug=slug)
