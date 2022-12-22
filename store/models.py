a7957dbe1805623ae5f06114bff327f1fd887a15
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-22 16:24:45 +0530

    Done with add thumbnails

--- store/models.py
+++ store/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files import File
+
+from io import BytesIO
+from PIL import Image
 # Create your models here.
 
 class Category(models.Model):
@@ -31,7 +35,8 @@ class Product(models.Model):
     slug = models.SlugField(max_length=50)
     description = models.TextField(blank=True)
     price = models.IntegerField()
-    image = models.ImageField(upload_to='uploads/product_images/',blank=True,null=True)
+    image = models.ImageField(upload_to='uploads/product_images',blank=True,null=True)
+    thumbnail = models.ImageField(upload_to='uploads/product_images/thumbnails',blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=50,choices=STATUS_CHOICES,default=ACTIVE)
@@ -45,3 +50,23 @@ class Product(models.Model):
     def get_display_price(self):
         return self.price/100
     
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                return self.thumbnail.url
+            else:
+                return 'https://via.placeholder.com/240x240x.jpg'
+    
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+        name = image.name.replace('uploads/product_images/','')
+        thumbnail = File(thumb_io, name=name)
+        return thumbnail
