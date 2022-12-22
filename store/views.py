a194522f19353f45e4410a56690fd7347fee0720
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-22 15:05:16 +0530

    Done with the increment and decrement with the product in the cart

--- store/views.py
+++ store/views.py
@@ -14,6 +14,18 @@ def remove_from_cart(request,product_id):
     cart.remove(product_id)
     return redirect('cart_view')
 
+def change_quantity(request,product_id):
+    action= request.GET.get('action','')
+    if action:
+        quantity=1
+        if action =='decrease':
+            quantity = -1
+        cart = Cart(request)
+        cart.add(product_id,quantity,True)
+    return redirect('cart_view')
+    
+        
+
 def cart_view(request):
     cart = Cart(request)
     return render(request,"store/cart_view.html",{'cart':cart})
